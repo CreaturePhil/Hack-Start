0528e78736c50bb55d446d672317ed04d0e6be31
Phil <birkal@outlook.com>
2015-05-30 23:11:15 -0700

    Remove deps: connect-assets

--- app.js
+++ app.js
@@ -20,7 +20,6 @@ var path = require('path');
 var mongoose = require('mongoose');
 var passport = require('passport');
 var expressValidator = require('express-validator');
-var connectAssets = require('connect-assets');
 
 /**
  * Controllers (route handlers).
@@ -56,9 +55,6 @@ app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.use(compress());
-app.use(connectAssets({
-  paths: [path.join(__dirname, 'public/css'), path.join(__dirname, 'public/js')]
-}));
 app.use(logger('dev'));
 app.use(favicon(path.join(__dirname, 'public/favicon.png')));
 app.use(bodyParser.json());
