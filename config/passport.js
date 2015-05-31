e010a8155de4ea4979c83af8602c120a40a9ffbc
Phil <birkal@outlook.com>
2015-05-31 16:53:50 -0700

    Passport

--- config/passport.js
+++ config/passport.js
@@ -1,8 +1,8 @@
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 
-var secrets = require('./secrets');
-var User = require('../models/User');
+var User = require('../app/models/User');
+var config = require('./index');
 
 passport.serializeUser(function(user, done) {
   done(null, user.id);
@@ -15,17 +15,17 @@ passport.deserializeUser(function(id, done) {
 });
 
 /**
- * Sign in using Email and Password.
+ * Sign in using Username and Password.
  */
-passport.use(new LocalStrategy({ usernameField: 'email' }, function(email, password, done) {
-  email = email.toLowerCase();
-  User.findOne({ email: email }, function(err, user) {
-    if (!user) return done(null, false, { message: 'Email ' + email + ' not found'});
+
+passport.use(new LocalStrategy({ usernameField: 'username' }, function(username, password, done) {
+  User.findOne({ uid: username.toLowerCase() }, function(err, user) {
+    if (!user) return done(null, false, { message: 'User ' + username + ' not found.' });
     user.comparePassword(password, function(err, isMatch) {
       if (isMatch) {
         return done(null, user);
       } else {
-        return done(null, false, { message: 'Invalid email or password.' });
+        return done(null, false, { message: 'Invalid username or password.' });
       }
     });
   });
@@ -34,8 +34,8 @@ passport.use(new LocalStrategy({ usernameField: 'email' }, function(email, passw
 /**
  * Login Required middleware.
  */
+
 exports.isAuthenticated = function(req, res, next) {
   if (req.isAuthenticated()) return next();
   res.redirect('/login');
 };
-
