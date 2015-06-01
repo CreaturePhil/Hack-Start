27990280d358b5a41bb3136d2e6f6aef3a7b4e1a
Phil <birkal@outlook.com>
2015-05-31 19:58:03 -0700

    No Reset Password

--- app/controllers/user_controller.js
+++ app/controllers/user_controller.js
@@ -1,10 +1,3 @@
-/**
- * Module dependencies.
- */
-
-var async = require('async');
-var crypto = require('crypto');
-var nodemailer = require('nodemailer');
 var passport = require('passport');
 
 var User = require('../models/User');
@@ -56,7 +49,6 @@ module.exports = {
   postSignup: function(req, res, next) {
     req.assert('username', 'Only letters and numbers are allow in username.').regexMatch(/^[A-Za-z0-9]*$/);
     req.assert('username', 'Username cannot be more than 30 characters.').len(1, 30);
-    req.assert('email', 'Email is not valid.').isEmail();
     req.assert('password', 'Password must be at least 4 characters long.').len(4);
 
     var errors = req.validationErrors();
@@ -69,27 +61,20 @@ module.exports = {
     var user = new User({
       uid: req.body.username.toLowerCase(),
       username: req.body.username,
-      email: req.body.email,
       password: req.body.password
     });
 
-    User.findOne({ email: req.body.email }, function(err, existingUser) {
+    User.findOne({ uid: req.body.username.toLowerCase() }, function(err, existingUser) {
       if (existingUser) {
-        req.flash('errors', { msg: 'Account with that email address already exists.' });
+        req.flash('errors', { msg: 'Account with that username already exists.' });
         return res.redirect('/signup');
       }
-      User.findOne({ uid: req.body.username.toLowerCase() }, function(err, existingUser) {
-        if (existingUser) {
-          req.flash('errors', { msg: 'Account with that username already exists.' });
-          return res.redirect('/signup');
-        }
-        user.save(function(err) {
+      user.save(function(err) {
+        if (err) return next(err);
+        req.logIn(user, function(err) {
           if (err) return next(err);
-          req.logIn(user, function(err) {
-            if (err) return next(err);
-            req.flash('success', { msg: 'Success! You account has been created.' });
-            res.redirect('/');
-          });
+          req.flash('success', { msg: 'Success! You account has been created.' });
+          res.redirect('/');
         });
       });
     });
--- app/models/User.js
+++ app/models/User.js
@@ -3,7 +3,6 @@ var crypto = require('crypto');
 var mongoose = require('mongoose');
 
 var userSchema = new mongoose.Schema({
-  email: { type: String, unique: true, lowercase: true },
   uid: { type: String, unique: true, lowercase: true },
   username: { type: String, unique: true },
   password: String,
