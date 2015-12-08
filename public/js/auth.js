bfe14754f6bdde4919a73d579ff429c2577e4d50
Phil <birkal@outlook.com>
2015-12-07 20:56:17 -0800

    Tokens

--- public/js/auth.js
+++ public/js/auth.js
@@ -1,4 +1,30 @@
-(function($) {
+(function($, window) {
+
+  const TOKEN_STORAGE_NAME = 'hack-start-token';
+
+  function saveToken(token) {
+    window.localStorage.setItem(TOKEN_STORAGE_NAME, token);
+  }
+
+  function getToken() {
+    return window.localStorage.getItem(TOKEN_STORAGE_NAME);
+  }
+
+  function isLoggedIn() {
+    var token = getToken();
+
+    if (token) {
+      var payload = JSON.parse(window.atob(token.split('.')[1]));
+
+      return payload.exp > (Date.now() / 1000);
+    }
+
+    return false;
+  }
+
+  function logout() {
+    window.localStorage.removeItem(TOKEN_STORAGE_NAME);
+  }
 
   $('form').on('submit', function(e) {
     e.preventDefault();
@@ -11,6 +37,7 @@
       dataType: 'json',
       success: function(data) {
         console.log(data);
+        saveToken(data.token);
       },
       error: function(err) {
         console.log(err);
@@ -18,4 +45,4 @@
     });
   });
 
-})(jQuery);
+})(jQuery, window);
